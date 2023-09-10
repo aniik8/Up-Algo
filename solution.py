0b3fea4de75cf2fd298d007f3eccb48bc4af47c9
Aniket <aniketmessi3@gmail.com>
2023-09-11 01:23:38 +0530

    stripe payment integration

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
-def palindrome(string):
-	new_str = ""
-	for i in string:
-		if i.isalnum():
-			new_str += (i)
-	return new_str.lower()[::-1] == new_str.lower()
+def nth_fib(n):
+	if n == 2:
+		return 1
+	elif n == 1:
+		return 0
+	else:
+		return nth_fib(n-1) + nth_fib(n-2)
