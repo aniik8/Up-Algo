debde2e5597cd1c7c59e1d0254bb0e161fd6e0e0
Aniket <aniketmessi3@gmail.com>
2023-10-03 01:37:31 +0530

    discussion

--- discussion/urls.py
+++ discussion/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', read_discussion, name="discussion")
+    path('', read_discussion, name="discussion"),
+    path('create-discussion', create_discussion, name="create_discussion")
 ]
